c729890a9fa02dc707da7581c7917bea4e932163
Viet Nguyen <viet.nguyen@csit.fi>
2020-03-01 19:56:13 +1100

    update db init script and add to travis

--- servian_twitter/__init__.py
+++ servian_twitter/__init__.py
@@ -12,7 +12,7 @@ with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.json'
     CONFIG = json.load(f)
 app.config['SECRET_KEY'] = CONFIG['SECRET_KEY']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///servian_twitter.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = CONFIG['SQLITE_URL']
 
 socketio = SocketIO(app)
 
--- servian_twitter/init_db.py
+++ servian_twitter/init_db.py
@@ -9,26 +9,35 @@
 #  > (sqlite3.IntegrityError) UNIQUE constraint failed: system_config.key
 #
 # -----------------------------------------------------------
+from sqlalchemy_utils import database_exists
+import os
+import json
 
 from servian_twitter.models import db, SystemConfig
 
-print ('Preparing to create database with some initial config settings...')
+print ('Preparing to create database with some initial config settings.')
 
-db.create_all()
+with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.json')) as f:
+    CONFIG = json.load(f)
 
-initial_config_values = {
-    'TWITTER_ACCESS_TOKEN': '1231806543030415360-8V3FC7LObpvgL7ckrewzdUjjnPuamd',
-    'TWITTER_ACCESS_SECRET': 'jRvpNwBKvqiNDhkVItSj4yyCohGQNQ79VSyyCFyW5GweZ',
-    'TWITTER_API_KEY': 'XJHHqegPrw0BohX8Grxp8lIOT',
-    'TWITTER_API_SECRET': '6tebbwIHTN3iHbUfrlW8bOLkKGcr6cjoFPNnwpAaqxuFaeeIKF',
-    'TWITTER_STREAM_KEYWORD': 'python',
-    'TWEET_QUERY_LIMIT': '100',
-}
+if not database_exists(CONFIG['SQLITE_URL']):
+    db.create_all()
 
-for key, value in initial_config_values.items():
-    config = SystemConfig(key=key, value=value)
-    db.session.add(config)
+    initial_config_values = {
+        'TWITTER_ACCESS_TOKEN': '1231806543030415360-8V3FC7LObpvgL7ckrewzdUjjnPuamd',
+        'TWITTER_ACCESS_SECRET': 'jRvpNwBKvqiNDhkVItSj4yyCohGQNQ79VSyyCFyW5GweZ',
+        'TWITTER_API_KEY': 'XJHHqegPrw0BohX8Grxp8lIOT',
+        'TWITTER_API_SECRET': '6tebbwIHTN3iHbUfrlW8bOLkKGcr6cjoFPNnwpAaqxuFaeeIKF',
+        'TWITTER_STREAM_KEYWORD': 'python',
+        'TWEET_QUERY_LIMIT': '100',
+    }
 
-db.session.commit()
+    for key, value in initial_config_values.items():
+        config = SystemConfig(key=key, value=value)
+        db.session.add(config)
 
-print ('Database created!')
+    db.session.commit()
+
+    print ('Database created!')
+else:
+    print ('Datbase already exists, now exiting.')
