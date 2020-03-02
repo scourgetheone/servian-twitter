44c4d44a7a91b8ba191d8b37f7afb8a7a60bc9f8
Viet Nguyen <viet.nguyen@csit.fi>
2020-03-02 15:05:38 +1100

    init the database in a startup script
    to circumvent issue with Travis CI not properly initializing the
    database.

--- servian_twitter/init_db.py
+++ servian_twitter/init_db.py
@@ -43,36 +43,6 @@ def init_db():
     else:
         print ('Datbase already exists, now exiting.')
 
-def copy_from_template():
-    """Copy a template starting database from the travis_templates folder
-
-    This is a workaround because the Travis CI build environment doesn't
-    seem to run the init_db() function above properly.
-    """
-    print ('Preparing to create a new database from an existing template.')
-
-    if not database_exists(CONFIG['SQLITE_URL']):
-        import shutil
-
-        # Source path
-        source = "travis_templates/servian_twitter_template.db"
-
-        # Destination path
-        destination = "servian_twitter.db"
-
-        # Copy the content of
-        # source to destination
-        dest = shutil.copyfile(source, destination)
-        print ('Database created from template!')
-    else:
-        print ('Datbase already exists, now exiting.')
-
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        if sys.argv[1] == 'from_template':
-            copy_from_template()
-        else:
-            init_db()
-    else:
-        init_db()
+    init_db()
---
+++ start_server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# start_server.sh: This script initializes the database
+# and then starts the Flask web server.
+#
+
+# test and bundle the react app in production mode.
+pushd servian_twitter/ >/dev/null
+python init_db.py
+popd >/dev/null
+
+if [ -z ${PORT+x} ]; then
+    PORT=5000
+fi
+
+gunicorn -b :$PORT -k eventlet servian_twitter:app
